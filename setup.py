3b4d33aa91b6a7670f22da484ec55b1f7dadfeb5
abhisek-13 <abhisekmaharana9861@gmail.com>
2025-06-15 18:44:20 +0530

    code structure done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Abhisek Maharana",
+    author_email="abhisekmaharana9861@gmail.com",
+    packages=find_packages()
+)
