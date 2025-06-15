4c6c9e42a6ed60805285c367f2f6737e0adc6859
abhisek-13 <abhisekmaharana9861@gmail.com>
2025-06-15 23:37:59 +0530

    logging module is added into the code.

---
+++ demo.py
@@ -0,0 +1,15 @@
+# below code is to check the logging config
+# from src.logger import logging
+
+# logging.debug("This is a debug message.")
+# logging.info("This is an info message.")
+# logging.warning("This is a warning message.")
+# logging.error("This is an error message.")
+# logging.critical("This is a critical message.")
+
+# --------------------------------------------------------------------------------
+
+# # below code is to check the exception config
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
