52dcb9280532ec751f564255abd07a7a85838c09
abhisek-13 <abhisekmaharana9861@gmail.com>
2025-06-28 23:29:44 +0530

    model training done and model object saved.

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -60,7 +60,7 @@ Model Trainer related constants start with `DATA_TRANSFORMATION' VAR NAME
 
 MODEL_TRAINER_DIR_NAME: str = "model_trainer"
 MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
-MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_FILE_NAME: str = "model.pkl"
 MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
 MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
 MODEL_TRAINER_N_ESTIMATORS: int = 100
---
+++ src/entity/estimator.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from pandas import DataFrame
+from sklearn.pipeline import Pipeline
+import sys
+from src.exception import MyException
+from src.logger import logging
+
+
+class MyModel:
+  def __init__(self,preprocessing_object: Pipeline, trained_model_object: object):
+    """
+    :param preprocessing_object: Input Object of preprocesser
+    :param trained_model_object: Input Object of trained model 
+    """
+    self.preprocessing_object = preprocessing_object
+    self.trained_model_object = trained_model_object
+    
+  def predict(self, dataframe: pd.DataFrame) -> DataFrame:
+    """
+    Function accepts preprocessed inputs (with all custom transformations already applied),
+    applies scaling using preprocessing_object, and performs prediction on transformed features.
+    """
+    
+    try:
+      logging.info("starting prediction process")
+      
+      transformed_feature = self.preprocessing_object.transform(dataframe)
+      
+      logging.info("Using the trained model to get the predictions.")
+      predictions = self.trained_model_object.predict(transformed_feature)
+      
+      return predictions
+    except Exception as e:
+      logging.info("Error occured during predict method",exc_info=True)
+      raise MyException(e,sys) from e
+    
+  def __repr__(self):
+        return f"{type(self.trained_model_object).__name__}()"
+
+  def __str__(self):
+      return f"{type(self.trained_model_object).__name__}()"
